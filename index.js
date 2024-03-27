d3065f6626c5be9035ed8d904b3dc0263c911049
ortegaerin@icloud.com <ortegaerin@icloud.com>
2024-03-26 23:55:17 -0500

    pushed project to GitHub

--- index.js
+++ index.js
@@ -1,13 +1,83 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "Please input your Project Name here.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please input description and purpose of your Project here."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please input installation instructions for your Project here.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please input how to use your Project here.",
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "Please input any collaborators on your Project here.",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please select a license that suits your Project here.",
+        choices: [
+            "Apache 2.0",
+            "GNU GPLv3.0",
+            "MIT",
+            "BSD 2",
+            "BSD 3",
+            "Boost Software 1.0",
+            "CCZ v1.0",
+            "EPL 2.0",
+            "GNU AGPLv3",
+            "GNU GPLv2.0",
+            "GNU LGPLv2.1",
+            "MPL 2.0",
+            "None",
+            ],
+    },
+    {
+        type: "input",
+        name: "creator",
+        message: "Please input GitHub username here."
+    },
+    {
+        type: "input",
+        name: "name",
+        message: "Please input your full name here."
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please input your email address here.",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, generateMarkdown(data))
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((data) => {
+        console.log(JSON.stringify(data, null, " "));
+        writeToFile("./README/README.md", data);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `![badge](https://img.shields.io/badge/license-${license})`;
+  }
+  return "";
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -13,7 +17,37 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Table of Contents
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Contact-Info](#contact-info)
+* [Tests](#tests)
 
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## License
+${data.license}
+
+## Contact-Info
+* Name - ${data.name}
+* Email - ${data.email}
+* GitHub - ${data.creator}
+
+## Tests
+${data.tests}
 `;
 }
 
